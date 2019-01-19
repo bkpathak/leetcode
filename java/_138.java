43ea9eacc022949f287b3be9c1fecccfd31a7049
Bijay Pathak <bkpathak@mtu.edu>
2019-01-19 09:19:26 -0800

    Deep copy of list

---
+++ java/_138.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list with a random pointer.
+ * class RandomListNode {
+ *     int label;
+ *     RandomListNode next, random;
+ *     RandomListNode(int x) { this.label = x; }
+ * };
+ */
+public class Solution {
+    public RandomListNode copyRandomList(RandomListNode head) {
+        if (head == null) return null;
+        
+        Map<RandomListNode, RandomListNode> map = new HashMap<>();
+        
+        RandomListNode curr = head;
+        
+        // copy all the nodes and put to map
+        while (curr != null) {
+            map.put(curr, new RandomListNode(curr.label));
+            curr = curr.next;
+        }
+        
+        // add next and random pointers
+        
+        curr = head;
+        
+        while (curr != null) {
+            map.get(curr).next = map.get(curr.next);
+            map.get(curr).random = map.get(curr.random);
+            curr = curr.next;
+        }
+        
+        
+        return  map.get(head);
+        
+        
+    }
+}
